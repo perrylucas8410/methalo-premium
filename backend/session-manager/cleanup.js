f378553dee5701c20f9fbcccba736c76867cf0f7
perrylucas8410 <129779480+perrylucas8410@users.noreply.github.com>
2026-03-20 04:12:35 -0500

    Delete backend directory

--- backend/session-manager/cleanup.js
+++
@@ -1,8 +0,0 @@
-function setupCleanup(sessions) {
-  setInterval(() => {
-    // TODO: add TTL / lastActive logic
-    // For now, do nothing
-  }, 60_000);
-}
-
-module.exports = { setupCleanup };
--- backend/utils/id.js
+++
@@ -1,7 +0,0 @@
-const { v4: uuid } = require("uuid");
-
-function newId() {
-  return uuid();
-}
-
-module.exports = { newId };
